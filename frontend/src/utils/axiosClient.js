4569b039a0901bd70224ae05949976779e16a9d3
Lucky <lopamudrashoo@gmail.com>
2025-08-01 11:45:02 +0530

    done

---
+++ frontend/src/utils/axiosClient.js
@@ -0,0 +1,23 @@
+// utils/axiosClient.js
+import axios from "axios"
+
+const axiosClient = axios.create({
+  baseURL:  "http://localhost:5000/",
+  headers: {
+    "Content-Type": "application/json",
+  },
+})
+
+// Automatically attach token if available
+axiosClient.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token")
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`
+    }
+    return config
+  },
+  (error) => Promise.reject(error)
+)
+
+export default axiosClient
