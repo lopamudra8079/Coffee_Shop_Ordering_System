527a6063c28db9a9543046d3bc76db9e8aba0df4
Lucky <lopamudrashoo@gmail.com>
2025-08-03 21:05:02 +0530

    Uploaded backend hosted url

--- frontend/src/utils/axiosClient.js
+++ frontend/src/utils/axiosClient.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000",
+  baseURL: "https://coffee-shop-ordering-system.onrender.com/",
   headers: {
     "Content-Type": "application/json",
   },
